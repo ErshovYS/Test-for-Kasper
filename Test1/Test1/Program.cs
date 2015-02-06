0cf73887f31d701a814eeb235b8339e704049f2b
Ершов Юрий Сергеевич (ИУ7-19) <yurikzz@narod.ru>
2015-02-06 13:15:44 +0300

    Test

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FileName;
+            if (args.Length > 0)
+            {
+                FileName = args[0];
+            }
+            else
+            {
+                Console.WriteLine("Введите адрес файла:");
+                FileName = Console.ReadLine();
+            }
+
+            StreamReader SR = new StreamReader(FileName, Encoding.Default);
+            string text = SR.ReadToEnd();
+            List<string> WordS = new List<string>(text.Split(new char[] { ' ', ',', '\n', '.', '\r' }));
+            SR.Close();
+
+            // Сортировка слов по длине
+            QuickS(WordS, 0, WordS.Count - 1);
+
+            int i = 0;
+            while (i < WordS.Count)
+            {
+                // Если найдены анаграммы слова, то они выводятся и удаляются вместе со словом
+                if (!Anagramma(WordS, i))
+                    i++;
+            }
+            Console.ReadKey();
+        }
+
+        // Быстрая сортировка
+        static void QuickS(List<string> WordS, int first, int last)
+        {
+            int i = first;
+            int j = last;
+            int mid = (last + first) / 2;
+            string bufS;
+
+            while (i <= j)
+            {
+                while (WordS[i].Length < WordS[mid].Length)
+                    i++;
+                while (WordS[j].Length > WordS[mid].Length)
+                    j--;
+
+                if (i <= j)
+                {
+                    if (i < j)
+                    {
+                        bufS = WordS[i];
+                        WordS[i] = WordS[j];
+                        WordS[j] = bufS;
+                    }
+                    i++;
+                    j--;
+                }
+            }
+
+            if (first < j)
+                QuickS(WordS, first, j);
+            if (i < last)
+                QuickS(WordS, i, last);
+        }
+
+        // Поиск анаграмм для слова под номером Num
+        static bool Anagramma(List<string> WordS, int Num)
+        {
+            int Len = WordS[Num].Length, NumWord = Num+1, Kol;  
+            bool[] ch = new bool[Len];                          // Совпал ли определенный символ
+            bool Annagr = false;                                // Есть ли у слова анаграммы
+
+            while (NumWord < WordS.Count && WordS[NumWord].Length == Len)
+            {
+                for (int i = 0; i < Len; i++)
+                    ch[i] = false;
+
+                Kol = 0;
+                for (int i = 0; i < Len; i++ )
+                {
+                    for (int j = 0; j < Len; j++)
+                    {
+                        if (!ch[j] && WordS[Num][i] == WordS[NumWord][j])
+                        {
+                            Kol++;
+                            ch[j] = true;
+                            break;
+                        }
+                    }
+                }
+                if (Kol == Len)
+                {
+                    Console.Write(WordS[NumWord] + " ");
+                    WordS.RemoveAt(NumWord);
+                    NumWord--;
+                    Annagr = true;
+                }
+                NumWord++;
+            }
+
+            if (Annagr)
+            {
+                Console.Write(WordS[Num] + "\n");
+                WordS.RemoveAt(Num);
+                return true;
+            }
+            else
+            { 
+                return false;
+            }
+        }
+    }
+}
---
+++ Test2/Test2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Test2
+{
+    class Program
+    {
+        static int count;                       // Количество строк в файле
+        static List<int> NumStr = new List<int>();                // Номера строк, в которых уже есть анаграммы
+
+        static void Main(string[] args)
+        {
+            string FileName;
+            if (args.Length > 0)
+            {
+                FileName = args[0];
+            }
+            else
+            {
+                Console.WriteLine("Введите адрес файла:");
+                FileName = Console.ReadLine();
+            }
+
+
+            count = File.ReadAllLines(FileName).Length;
+
+            int Num1 = 0;
+            string Word1, Word2, LineS;
+            while (Num1 < count)
+            {
+                if (NumStr.IndexOf(Num1) < 0)
+                {
+                    Word1 = File.ReadLines(FileName, Encoding.Default).Skip(Num1).First();
+                    LineS = "";
+                    int Num2 = Num1 + 1;
+
+                    while (Num2 < count)
+                    {
+                        if (NumStr.IndexOf(Num2) < 0)
+                        {
+                            Word2 = File.ReadLines(FileName, Encoding.Default).Skip(Num2).First();
+
+                            if (Word1.Length == Word2.Length)
+                            {
+                                if (Anagramma(Word1, Word2, ref LineS))
+                                {
+                                    if (NumStr.IndexOf(Num1) < 0)
+                                        NumStr.Add(Num1);
+                                    NumStr.Add(Num2);
+                                }
+                            }
+                        }
+                        Num2++;
+                    }
+                    if (NumStr.IndexOf(Num1) >= 0)
+                    {
+                        Console.WriteLine(LineS);
+                    }
+                }
+                Num1++;
+            }
+            Console.ReadKey();
+        }
+
+        // Сравнение слов и запись анаграмм в строку
+        static bool Anagramma(string Word1, string Word2, ref string LineS)
+        {
+            int Len = Word1.Length, Kol;
+            bool[] ch = new bool[Len];                          // Совпал ли определенный символ
+
+            for (int i = 0; i < Len; i++)
+                ch[i] = false;
+
+            Kol = 0;
+            for (int i = 0; i < Len; i++)
+            {
+                for (int j = 0; j < Len; j++)
+                {
+                    if (!ch[j] && Word1[i] == Word2[j])
+                    {
+                        Kol++;
+                        ch[j] = true;
+                        break;
+                    }
+                }
+            }
+            if (Kol == Len)
+            {
+                if (LineS.Length > 0)
+                    LineS += Word2 + " ";
+                else
+                    LineS += Word1 + " " + Word2 + " ";
+                return true;
+            }
+            return false;
+        }
+    }
+}
